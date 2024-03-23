3f6c72452002313b9582bf67ae13849d0943dabd
nephrite <oneabove232@gmail.com>
2024-03-23 13:25:53 +0800

    small ui fix

--- src/components/portfolio/CoinList.tsx
+++ src/components/portfolio/CoinList.tsx
@@ -1,30 +1,10 @@
-import { FaPlus } from "react-icons/fa6";
-import { Button } from "../ui/button";
+import Controls from "./Controls";
 import CoinCard from "./CoinCard";
 
 export  default function CoinList() {
     return (
         <section className="py-4 flex flex-col gap-y-4">
-            <div className="flex flex-row justify-end gap-x-2">
-                <Button variant={"ghost"} size={"icon"} className="scale-125">
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="16" height="7" rx="2" fill="#1E1E1E"/>
-                        <rect y="9" width="16" height="7" rx="2" fill="#1E1E1E"/>
-                    </svg>
-                </Button>
-                <Button variant={"ghost"} size={"icon"} className="scale-125">
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="7" height="7" rx="2" fill="#1E1E1E"/>
-                        <rect x="9" width="7" height="7" rx="2" fill="#1E1E1E"/>
-                        <rect y="9" width="7" height="7" rx="2" fill="#1E1E1E"/>
-                        <rect x="9" y="9" width="7" height="7" rx="2" fill="#1E1E1E"/>
-                    </svg>
-                </Button>
-                <Button className="gap-x-1 rounded-full px-4">
-                    <FaPlus className="fill-custom-teal" />
-                    <p className="text-sm font-[500]">Add</p>
-                </Button>
-            </div>
+            <Controls />
             <div className="flex flex-row flex-wrap">
                 {portfolioCoins.map((p,i) => {
                     return (
