2a11da9ac48c646dd6df2e473847946ee80994e5
nephrite <oneabove232@gmail.com>
2024-04-02 09:24:50 +0800

    coinlist prev value loading

--- src/components/portfolio/CoinList.tsx
+++ src/components/portfolio/CoinList.tsx
@@ -1,18 +1,20 @@
 import Controls from "./Controls";
 import CoinCard from "./CoinCard";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import CoinAccordion from "./CoinAccordion";
 import { Await, useLoaderData } from "react-router-dom";
 import { PortfolioItem } from "@/types";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import CoinListError from "./CoinListError";
 import { listTypeStore } from "@/hooks/useListType";
+import { usePrevData } from "@/hooks/usePrevData";
 
 export  default function CoinList() {
     const {data} = useLoaderData() as {data: [Promise<any>, Promise<any>],}
     const listType = listTypeStore(s => s.listType)
     const toggleListType = listTypeStore(s => s.toggleListType)
-
+    const prevPortfolio = usePrevData(s => s.prevPortfolio)
+    const setPrevPortfolio = usePrevData(s => s.setPrevPortfolio)
 
 
     return (
@@ -22,17 +24,32 @@ export  default function CoinList() {
             listType === 'card' ? 
             <div className="flex flex-row flex-wrap">
                 <Suspense fallback={
+                    prevPortfolio ?
+                    <>
+                    {prevPortfolio.map((p,i) => {
+                        return (
+                            <CoinCard key={p.id} {...p} index={i} />    
+                        )
+                    })}
+                    </> :
                     <div className="h-[100px] w-full flex items-center justify-center">
                         <div className="text-3xl">
                             <AiOutlineLoading3Quarters className="animate-spin" />
                         </div>  
                     </div>
+                    
             }
                 >
                     <Await resolve={data} errorElement={<CoinListError />}>
                         
                         {(res) => {
                             const portfolioCoins : PortfolioItem[] = res[1].data.data
+
+                            useEffect(() => {
+                                console.log('coinlist prevportfolio effect')
+                                setPrevPortfolio(portfolioCoins)
+                            }, [portfolioCoins])
+
                             return (
                                 <>
                                 {portfolioCoins.map((p,i) => {
@@ -48,6 +65,14 @@ export  default function CoinList() {
             </div> :            
             <div className="w-full">
                 <Suspense fallback={
+                    prevPortfolio ?
+                    <>
+                    {prevPortfolio.map((p,i) => {
+                        return (
+                            <CoinAccordion key={p.id} {...p} index={i} />    
+                        )
+                    })}
+                    </> :
                     <div className="h-[100px] w-full flex items-center justify-center">
                         <div className="text-3xl">
                             <AiOutlineLoading3Quarters className="animate-spin" />
